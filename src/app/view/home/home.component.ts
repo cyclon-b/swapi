180b6c1c3fd674c4229020f872e667e34ca2e227
Zyklon B <ahrimantaroth@gmail.com>
2023-05-31 00:23:56 +0300

    started work on home

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'swapi-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
